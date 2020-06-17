44131f2c3903c29f24ceeadec307d72d301eae63
macjabeth <macbethjonathan@gmail.com>
2020-06-17 16:18:34 -0400

    complete single number challenge

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,14 +2,15 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
+from functools import reduce
 
-    pass
+def single_number(arr):
+    return reduce(lambda t, n: t ^ n, arr)
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    
 
     print(f"The odd-number-out is {single_number(arr)}")
